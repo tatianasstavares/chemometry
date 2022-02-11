3b92f6a036a12fe41c13bb50acbfb7d0e2177648
Tatiana <tatianasstavares@gmail.com>
2022-02-11 15:14:05 +0000

    Adjustments  Styled Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react'
-import GlobalStyle from './styles/global'
+import React, { Component } from "react";
+import GlobalStyle from "./styles/global";
 export default class App extends Component {
-    render() {
-        return <GlobalStyle / >
-
-    }
+  render() {
+    return (
+      <div>
+        <GlobalStyle />
+      </div>
+    );
+  }
 }
--- src/components/Upload/index.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from 'react';
-
-export default class Upload extends Component {
-    render() {
-        return <div / > ;
-    }
-}
