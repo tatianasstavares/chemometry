4e67da8115b597985b9385c95c5e81079f6450c2
Tatiana <tatianasstavares@gmail.com>
2022-02-11 15:09:01 +0000

    Add Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-function App() {
-  return (
-    <div className="App">
-      Chemometry
-    </div>
-  );
-}
+import React, { Component } from 'react'
+import GlobalStyle from './styles/global'
+export default class App extends Component {
+    render() {
+        return <GlobalStyle / >
 
-export default App;
+    }
+}
